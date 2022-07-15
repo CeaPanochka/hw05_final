faae4403c72d1385c493e639beef06d16d9b3447
Дмитрий Цыбулаев <titampeach@gmail.com>
2022-07-15 13:51:15 +0300

    Исправления после ревью 2

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -3,6 +3,7 @@ from django.core.cache import cache
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import Client, TestCase
 from django.urls import reverse
+
 from posts.models import Follow, Group, Post, User
 
 FIRST_COUNT_PAGE_OBJ = 10
